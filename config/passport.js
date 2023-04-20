d9e68cf0ff7f9df39691189bb7fb5bd5fdd80852
rafaelbke <rafael.bke@gmail.com>
2023-04-20 16:40:28 -0300

    finalizando1

---
+++ config/passport.js
@@ -0,0 +1,26 @@
+//aula 367 token se segurança ,protegendo a api com passport
+const { authSecret } = require('../.env')
+const passport = require('passport')
+const passportJwt = require('passport-jwt')
+const { Strategy, ExtractJwt } = passportJwt
+
+module.exports = app => {
+    const params = {
+        secretOrKey: authSecret,
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+    }
+
+    const strategy = new Strategy(params, (payload, done) => {
+        app.db('users')
+            .where({ id: payload.id })
+            .first()
+            .then(user => done(null, user ? { ...payload } : false))
+            .catch(err => done(err, false))
+    })
+
+    passport.use(strategy)
+
+    return {
+        authenticate: () => passport.authenticate('jwt', { session: false })
+    }
+}
